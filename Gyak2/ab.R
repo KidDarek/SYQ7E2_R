7042c59d2c6ac6b06b03041ba60f8837f5df58e2
KidDarek <90865592+KidDarek@users.noreply.github.com>
2024-02-28 10:52:04 +0100

    Add files via upload

---
+++ Gyak2/ab.R
@@ -0,0 +1,46 @@
+szum <- function(a){
+    s <- 0
+    n <- length(a)
+    for(i in 1:n){
+        s <- s + a[i]
+    }
+    return(s)
+}
+ 
+ 
+szum2 <- function(a,b){
+    s <- 0
+    n <- length(a)
+    for(i in 1:n){
+        s <- s + a[i]* b[i]
+    }
+    return(s)
+}
+ 
+deter <- function(a,b,c,d){
+    return(a*d - b*c)
+}
+ 
+megoldas <- function(){
+    n <- 5000
+    x <- rnorm(n)
+    y <- x + rnorm(n)
+    sx <- szum(x)
+    sxy <- szum2(x,y)
+    sy <- szum(y)
+    sxx <- szum2(x,x)
+ 
+    b <- deter(sx,sxy,sx,sy) / deter(sxx,-sx,sx,n)
+    a <- deter(sxy,-sx,sy,n) / deter(sxx,-sx,sx,n)
+    print(a)
+    print(b)
+	px<- c(0,1)
+	py<- (a*px+b)
+	print(py)
+
+ 	plot(px,py,"l")
+	par(new=TRUE)
+ 	plot(x,y)
+}
+ 
+megoldas()
