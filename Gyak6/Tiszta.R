3a915e281b1723da52da837af0b4b6f37e650426
KidDarek <90865592+KidDarek@users.noreply.github.com>
2024-03-20 10:46:48 +0100

    Add files via upload

---
+++ Gyak6/Tiszta.R
@@ -0,0 +1,13 @@
+tiszta <- function(a,p){
+s <- 0
+n <- 600
+	for( i in 1:length(a))
+	{
+	s <- s + ((a[i]-(n*p[i]))^2)/(n*p[i])
+	}
+return(s)
+}
+
+a <- c(83,91,122,107,74,123)
+p <- c(1/6,1/6,1/6,1/6,1/6,1/6)
+tiszta(a,p)
